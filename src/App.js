3f73b927ce3e495323af38c91fd6936ed7769eb0
falak Chandni <falak.chandni@codingninjas.com>
2022-09-27 15:44:47 +0530

    babel preset import

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 data-testid="name">shubham</h1>
+      <h4> This is a simple project made using React </h4>
+      <p data-testid="info">Skills usbh to make this project</p>
+      <ol data-testid="skills">
+        <li>HTML</li>
+        <li>javaScript</li>
+        <li>React</li>
+      </ol>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,43 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, cleanup } from "@testing-library/react";
+// import screen from "@testing-library/react";
+// import cleanup from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(() => {
+  cleanup(); // Resets the DOM after each test suite
+});
+
+describe("HTML Elements", () => {
+  test("Heading exists", () => {
+    render(<App />);
+    const element = screen.getByTestId("name");
+    expect(element).toBeInTheDocument();
+    expect(element.nodeName).toBe("H1");
+  });
+
+  test("Paragraph exists", () => {
+    render(<App />);
+    const element = screen.getByTestId("info");
+    expect(element).toBeInTheDocument();
+    expect(element.nodeName).toBe("P");
+  });
+
+  test("List exists", () => {
+    render(<App />);
+    const element = screen.getByTestId("skills");
+    expect(element).toBeInTheDocument();
+    let flag = false;
+    if (element.nodeName === "UL" || element.nodeName === "OL") flag = true;
+    expect(flag).toBe(true);
+  });
+  test("List consists for 3 skills", () => {
+    render(<App />);
+    const element = screen.getByTestId("skills");
+    const items = element.children;
+    expect(items.length).toBe(3);
+    for (let i = 0; i < 3; i++) {
+      expect(items[i].nodeName).toBe("LI");
+    }
+  });
 });
